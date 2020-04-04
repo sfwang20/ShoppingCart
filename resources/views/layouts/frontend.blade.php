c6158ef56b74c3c48c82fb993ac316499c0dc558
Shin-Fu <shinfu619@gmail.com>
2020-04-04 09:18:44 +0800

    + index view + layouts view update routes

---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('layouts.head')
+
+<body>
+
+  @include('layouts.preloader')
+
+  <div class="wrapper">
+
+  @include('layouts.header')
+
+      @yield('page-title')
+
+      <section class="body-content ">
+
+        @yield('content')
+
+      </section>
+
+      @include('layouts.footer')
+
+  </div>
+
+
+  @include('layouts.js')
+  @yield('script')
+</body>
+
+</html>
