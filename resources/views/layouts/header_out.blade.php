f8ca87be1fb9bdc90b0c0ddd3b8f4ffb8799fb97
Shin-Fu <shinfu619@gmail.com>
2020-04-13 14:34:07 +0800

    add another header_out to show log out status view

--- resources/views/layouts/header_out.blade.php
+++ resources/views/layouts/header_out.blade.php
@@ -1,4 +1,3 @@
-<!--header start-->
 <header class="l-header">
   <div class="l-navbar l-navbar_t-light l-navbar_expand js-navbar-sticky">
     <div class="container-fluid">
@@ -12,50 +11,41 @@
 
         <!--mega menu start-->
         <ul class="menuzord-menu menuzord-right c-nav_s-standard">
-
           <li class="@if(request()->is('/')) active @endif"><a href="/">Home</a></li>
           <li class="@if(request()->is('products')) active @endif"><a href="/products">Shop</a></li>
+
           <li class="nav-divider" aria-hidden="true"><a href="javascript:void(0)">|</a></li>
 
           <li class="cart-info">
-            <a href="javascript:void(0)"><i class="fa fa-shopping-cart"></i> cart({{ $cart->products()->sum('quantity') }})</a>
+            <a href="javascript:void(0)"><i class="fa fa-shopping-cart"></i></a>
             <div class="megamenu megamenu-quarter-width ">
               <div class="megamenu-row">
                 <div class="col12">
 
                   <!--cart-->
                   <table class="table cart-table-list table-responsive">
-                  @if($cart->products())
-                    @foreach($cart->products as $key => $product)
+
                     <tr>
+                      <td></td>
+                      <td></td>
                       <td>
-                        <a href="/products/{{ $product->id }}">
-                          <img src="{{ $product->image}}" alt="" />
-                        </a>
-                      </td>
-                      <td><a href="/products/{{ $product->id }}"> {{$product->name}} </a>
-                      </td>
-                      <td>X{{ $product->pivot->quantity }}</td>
-                      <td>${{ $product->price * $product->pivot->quantity }}</td>
-                      <td>
-                        <a onclick="deleteCartItem( {{$product->id}} )" class="close">
+                        <a onclick="" class="close">
                           <img src="/assets/img/product/close.png" alt="" />
                         </a>
                       </td>
                     </tr>
-                    @endforeach
-                  @endif
+
                   </table>
 
                   <div class="total-cart pull-right">
                     <ul>
-                      <li><span>Total </span>  <span>$ {{$total}} </span>
+                      <li><span>Total </span>  <span></span>
                       </li>
                     </ul>
                   </div>
                   <div class="s-cart-btn pull-right">
                     <a href="/carts" class="btn btn-small btn-theme-color"> View cart</a>
-                    <a href="/carts" class="btn btn-small btn-dark-solid"> Checkout</a>
+                    <a href="#" class="btn btn-small btn-dark-solid"> Checkout</a>
                   </div>
                   <!--cart-->
 
@@ -65,14 +55,14 @@
           </li>
 
           <li>
-            <a class="dropdown-item" href="{{ route('logout') }}"
-               onclick="event.preventDefault();
-                             document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
-            </a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                @csrf
-            </form>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+            @endif
           </li>
         </ul>
         <!--mega menu end-->
@@ -82,4 +72,3 @@
   </div>
 
 </header>
-<!--header end-->
