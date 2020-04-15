05a04806710f7b07a05c7f2ce5b116ecb5ab644e
Shin-Fu <shinfu619@gmail.com>
2020-04-15 18:27:57 +0800

    add SoftDeletes and column 'status' fillable

--- app/Order.php
+++ app/Order.php
@@ -3,10 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Order extends Model
 {
-  protected $fillable = ['amount', 'address', 'creditCardNum', 'coupon', 'region', 'country'];
+  use SoftDeletes;
+  protected $dates = ['deleted_at'];
+
+  protected $fillable = ['amount', 'address', 'creditCardNum', 'coupon', 'region', 'country', 'status'];
   public function user()
   {
     return $this->belongsTo('App\User');
