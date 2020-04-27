7410b48172d8dae2cd49cc7f003bca0bcf929c66
Shin-Fu <shinfu619@gmail.com>
2020-04-27 19:50:14 +0800

    Finish Create and Delete ajax call

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -23,21 +23,26 @@ class CartController extends Controller
     $cart = $this->getCart();
     //compare products in user's cart
     $product = $cart->products->find($request->id);
-    //if cart has request's product -> find it and +1
+    //if cart has request's product -> find it and +q
+
     if ($product != null && $product->id == $request->id) {
-      $product->pivot->quantity += $request->quantity;
+      $product->pivot->quantity += (int)$request->quantity;
       $quantity = $product->pivot->quantity;
       $cart->products()->detach($request->id);
       $cart->products()->save($product, ['quantity' => $quantity]);
+      $firstTime = false;
     }
     //else  use attach() to create a new row in cart_product table
     else {
       $cart->products()->attach($request->id, ['quantity' => $request->quantity]);
+      $firstTime = true;
     }
 
     $cart->load('products');
     $product = $cart->products->find($request->id);
-    $product['add_quantity'] = $request->quantity;
+
+    $product['add_quantity'] = (int)$request->quantity;
+    $product['firstTime'] = $firstTime;
 
     return response()->json($product);
   }
@@ -55,6 +60,8 @@ class CartController extends Controller
   {
     $cart = $this->getCart();
     $cart->products()->detach($request->id);
+
+    return response()->json($request);
   }
 
   public function getCart()
